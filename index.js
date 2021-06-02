433cbd3dcfd361c0405eb1b511355d8d1476518d
alicewaweru28 <80792502+alicewaweru28@users.noreply.github.com>
2021-06-01 18:29:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,76 @@
+// Packages and files required for this project
+const inquirer = require("inquirer");
 const fs = require("fs");
-const path = require("path");
-const inquirer = require('inquirer');
-const generateMarkdown = require("./utils/generateMarkdown");
+const util = require('util');
+const generateMD = require('./scripts/generateMarkdown.js');
 
-//Array of questions for users to generate the Readme.
-function promptUser() {
+// Creates file Asynchronously (we used this structure in class)
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Questions awaiting user input
+const questions = () => {
     return inquirer.prompt([
-    {
-        type:"input",
-        message: "Project Title",
-        Name: "title",
-    },
-    { 
-        type: "input",
-        message: "Description of the Project?",
-        Name: "Description",
-    },
-    {  
-        type: "input",
-         message: "run a test on your application?",
-          name: "tests",
-    },
-    { 
-        type: "input",
-        message: " email address?",
-        name: "email"
-    },
-    {
-        type:"input",
-        message: "contributors of the project?",
-        name:"contributuion",
-    },
-    { 
-        type: "input",
-        message: "Repository name?",
-        name: "repo"
-    },
-    { 
-        type: "input",
-        message: "Instructions on how to use you project?",
-        name: "usage",
-    },
-    { 
-        type: "input",
-        message: " GitHub username?",
-        name: "github"
-    },
-    {
-        type: "input",
-        message: "installation instructions for this project?",
-        name: "installation",
-    },
-    {
-        type: "checkbox",
-        message: "license project",
-        choices:[
-            "Existing license: ORIGINAL",
-          "MIT",
-          "ISC",
-          "GNU GPLv3",
-          "None"
-        ]
-    },
-    { 
-        type: "input",
-        message: "testing instructions for this project",
-        name: "testing",
-    }
-]);
-}
+        {
+            type: 'input',
+            name: 'welcome',
+            message: 'Welcome to my automated README.md Generator. \n I\'m going to ask you some questions that will help me complete your customized README.md. \n Let\'s get started! Press ENTER to continue.',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project, exactly as it appears on GitHub?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Input your project description.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Input your project installation instructions.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Input your project usage information.',
+             },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Input your project contribution guidelines.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Input your project test instructions.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license applies to your project?',
+            choices: ['Apache-2.0', 'BSD-2-Clause', 'BSD-3-Clause', 'CDDL-1.0', 'EPL-2.0', 'GPL-2.0', 'LGPL-3.0', 'MIT', 'MPL-2.0', 'N/A'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address.',
+        },
+    ]);
+};
 
-//This function runs after the prompts are answered to generateMarkdown function and write the READme.
+// Function to initialize app
+const init = () => {
+    questions()
+        .then((answers) => writeFileAsync('./output/README.md', generateMD(answers)))
+        .then(() => console.log('Successfully wrote READMD.md to your output folder'))
+        .catch((err) => console.error(err));
+};
 
-function init() {
-    try {
-      const answers =  promptUser();
-      const markDown = generateMarkdown(answers);
-      onsole.log(markDown);
-      console.log(answers);
-  
-      await writeFileAsync(outputPath, markDown);
-  
-      console.log("Successfully wrote to README.md");
-    } catch(err) {
-      console.log(err);
-    }
-  }
-  
-  init();
+// Function call to initialize app
+init();
