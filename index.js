d6268338484728bd69a4256efd516cbf8881a58a
alicewaweru28 <80792502+alicewaweru28@users.noreply.github.com>
2021-06-07 13:37:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,103 @@
-// Packages and files required for this project
+// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require('util');
-const generateMD = require('./scripts/generateMarkdown.js');
+// TODO: Create an array of questions for user input
+const questions = [
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please add a short description of your project:",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What installation is this?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is the usage?",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Please choose the license used for this project:",
+        choices: ["MIT", "GNU", "GPL", "Other", "None"],
+      },
+      {
+        type: "input",
+        name: "contributers",
+        message: "Please include the contributers to your project:",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What are the test instructions for your project?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+    ])
+    .then((answers) => {
+      fs.writeFile("README.md", writeToFile(answers), (err) =>
+        err ? console.log(err) : console.log("Successfully created index.html!")
+      );
+    }),
+];
 
-// Creates file Asynchronously (we used this structure in class)
-const writeFileAsync = util.promisify(fs.writeFile);
+// console.log(questions);
 
-// Questions awaiting user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'welcome',
-            message: 'Welcome to my automated README.md Generator. \n I\'m going to ask you some questions that will help me complete your customized README.md. \n Let\'s get started! Press ENTER to continue.',
-        },
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project, exactly as it appears on GitHub?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Input your project description.',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Input your project installation instructions.',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Input your project usage information.',
-             },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'Input your project contribution guidelines.',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Input your project test instructions.',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Which license applies to your project?',
-            choices: ['Apache-2.0', 'BSD-2-Clause', 'BSD-3-Clause', 'CDDL-1.0', 'EPL-2.0', 'GPL-2.0', 'LGPL-3.0', 'MIT', 'MPL-2.0', 'N/A'],
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub username.',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email address.',
-        },
-    ]);
-};
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+const writeToFile = (answers) =>
+  `# ${answers.title} 
+  # Description 
+  ${answers.description} 
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)  
+  * [License](#license)
+  * [Contributers](#contributers)
+  * [Test Instructions](#test-instructions)
+  * [Questions](#questions)
+  
+  # Installation
+  <a name="installation"></a>
+  * ${answers.installation} 
+  # Usage
+  <a name="usage"></a>
+  * ${answers.usage} 
+  # License
+  <a name="license"></a>
+  * ${answers.license} 
+  # Contributers
+  <a name="contributers"></a>
+  * ${answers.contributers} 
+  # Test Instructions
+  <a name="test-instructions"></a>
+  * ${answers.test} 
+  # Questions
+  <a name="questions"></a>
+  * [Github Link](https://github.com/${answers.github})
+  * For additional questions, please contact me via email at ${answers.email}
+ `;
 
-// Function to initialize app
-const init = () => {
-    questions()
-        .then((answers) => writeFileAsync('./output/README.md', generateMD(answers)))
-        .then(() => console.log('Successfully wrote READMD.md to your output folder'))
-        .catch((err) => console.error(err));
-};
+// TODO: Create a function to initialize app
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
